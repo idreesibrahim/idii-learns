e13f87d7ebd7ad816a1fd25c277167490a63a31e
Idrees Ibrahim <idrees.ibrahim@pitb.gov.pk>
2022-08-07 17:27:30 +0500

    props drilling react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import ExpenseItem from './components/ExpenseItem'
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date('2022, 8, 1')
+    },
+    {
+      id: 2,
+      title: 'Car Insurance',
+      amount: 2000000.0,
+      date: new Date('2022, 8, 2')
+    },
+    {
+      id: 3,
+      title: 'New Desk',
+      amount: 10000.0,
+      date: new Date('2022, 8, 5')
+    }
+
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
     </div>
   );
 }
